c19e66f814b613ef90d6c90a07e5e7d3fb601743
Konrad <konradkarimi@gmail.com>
2020-06-01 15:57:25 +0200

    Some constrains on layout and file reading

--- app/src/main/java/com/konradkarimi/bskapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/konradkarimi/bskapp/ui/home/HomeFragment.java
@@ -1,6 +1,11 @@
 package com.konradkarimi.bskapp.ui.home;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +22,16 @@ import androidx.lifecycle.ViewModelProviders;
 import com.konradkarimi.bskapp.R;
 import com.konradkarimi.bskapp.databinding.FragmentHomeBinding;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 public class HomeFragment extends Fragment {
+
+    private static final int PICK_TXT_FILE = 1;
+
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
@@ -32,6 +46,12 @@ public class HomeFragment extends Fragment {
         binding.setViewmodel(homeViewModel);
         binding.setLifecycleOwner(this);
         View view = binding.getRoot();
+        binding.encryptBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFile(Uri.parse("download"));
+            }
+        });
         return view;
     }
 
@@ -39,5 +59,50 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+
+    private void openFile(Uri pickerInitialUri) {
+        System.out.println("openfile");
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/*");
+
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, PICK_TXT_FILE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_TXT_FILE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+                System.out.println("This is uri:" + uri);
+//                homeViewModel.setUri(uri);
+                try {
+                    String fileText = readTextFromUri(uri);
+                    homeViewModel.setText(fileText);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private String readTextFromUri(Uri uri) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
